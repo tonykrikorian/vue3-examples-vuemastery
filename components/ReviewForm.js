5122ce6bf5c094b14bd652d291bcc5adcf3572c2
Tony Krikorian <tony.krikorian@anschile.cl>
2022-04-07 15:14:02 -0400

    Add components and form

---
+++ components/ReviewForm.js
@@ -0,0 +1,56 @@
+app.component("review-form", {
+  template:
+    /*html*/
+    `<form class="review-form" @submit.prevent="onSubmit">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" >
+
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review" ></textarea>
+
+    <label for="recommend">Would you recommend this product?</label>
+    <input id="recommend" v-model="recommend"  />
+    
+
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating" >
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+
+    <input class="button" type="submit" value="Submit"  >
+  </form>`,
+  data() {
+    return {
+      name: "",
+      review: "",
+      rating: null,
+      recommend: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (!this.name || !this.review || !this.rating) {
+        alert("Debes rellenar todos los campos");
+        return;
+      }
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+        recommend: this.recommend || "",
+      };
+
+      this.$emit("review-submitted", productReview);
+
+      this.name = "";
+      this.review = "";
+      this.rating = "";
+      this.recommend = "";
+    },
+  },
+});
